bad3649927dcf44533d21b111506447b75f93dd6
wczosnow <219902@student.agh.edu.pl>
2025-01-08 01:25:51 +0100

    lab 2 task 2

---
+++ lab2/task_2.cc
@@ -0,0 +1,28 @@
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Enter a positive number: ");
+	int32_t input;
+	scanf("%" PRId32, &input);
+	if(input > 0)
+	{
+		//int64_t sum = input * (input + 1) * 0.5f;
+		int64_t sum = 0;
+		for(size_t i = 1; i <= (size_t)input; ++i)
+		{
+			sum += i;
+		}
+		printf("%" PRId64 "\n", sum);
+
+	}
+	else
+	{
+		printf("Number is not positive (negative or zero)!\n");
+	}
+	
+	return 0;
+}
+
